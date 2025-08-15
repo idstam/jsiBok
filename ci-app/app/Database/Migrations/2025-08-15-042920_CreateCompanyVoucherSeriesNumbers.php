c8ae8a3b46d9c780e5da5ed77265a0567e079880
johan idstam <johan@idstam.com>
2025-08-15 06:37:24 +0200

    Add company_voucher_series_numbers

---
+++ ci-app/app/Database/Migrations/2025-08-15-042920_CreateCompanyVoucherSeriesNumbers.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCompanyVoucherSeriesNumbers extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'company_id'          => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+            ],
+            'booking_year_id'          => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+            ],
+            'voucher_series_id'          => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+            ],
+            'next'          => [
+                'type'           => 'INT',
+                'unsigned'       => true,
+                'default'       => 1
+            ],
+            'created_at'       => [
+                'type'       => 'DATETIME',
+                'null'        => true,
+            ],
+            'updated_at'       => [
+                'type'       => 'DATETIME',
+                'null'        => true,
+            ],
+            'deleted_at'       => [
+                'type'       => 'DATETIME',
+                'null'        => true,
+            ],
+
+        ]);
+        $this->forge->addPrimaryKey(['id']);
+        $this->forge->addForeignKey('company_id', 'companies', 'id');
+        $this->forge->addForeignKey('booking_year_id', 'company_booking_years', 'id');
+        $this->forge->addForeignKey('voucher_series_id', 'company_voucher_series', 'id');
+        $this->forge->addUniqueKey(['company_id', 'booking_year_id', 'voucher_series_id'], 'company_voucher_series_numbers_UDX');
+        $this->forge->createTable('company_voucher_series_numbers');
+
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('company_voucher_series_numbers');
+
+    }
+}
